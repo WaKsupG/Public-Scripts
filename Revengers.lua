4bb75598b2e7f213be0cc7b295821d837dc08778
10x00 <75948662+10x00@users.noreply.github.com>
2021-12-12 10:57:17 -0700

    Create Revengers.lua

---
+++ Revengers.lua
@@ -0,0 +1,44 @@
+local create = loadstring(game:HttpGet("https://raw.githubusercontent.com/10x00/Tools/main/Yes%20UI%20Lib"))()
+
+local Window = create:Win("Sex Man")
+create:Notifile("10x UwU Rawr","I cum in ur mom hahahaha",3)
+local Tap1 = Window:Taps("Revengers")
+local page1 = Tap1:newpage()
+page1:Label("Auto-Train")
+page1:Line()
+
+local Stats = game:GetService("Players").LocalPlayer.Stats
+
+
+page1:Toggle("Auto Train",false,function(value)
+    _G.AutoMill = value
+
+    while _G.AutoMill and wait() do
+       for i,v in pairs(game:GetService("Workspace").Game.WorkOut:GetDescendants()) do
+           if game:GetService("Players").LocalPlayer.Stats.Stamina.Value == game.Players.LocalPlayer.Character.INFO.MaxStamina.Value then
+            if _G.Stat == "Treadmill" then
+                fireclickdetector(game:GetService("Workspace").Game.WorkOut.Treadmill.Platform.ClickDetector)
+                elseif _G.Stat == "Push Up" then
+                    fireclickdetector(game:GetService("Workspace").Game.WorkOut.PushUp.Part.ClickDetector)
+                elseif _G.Stat == "Squat" then
+                    fireclickdetector(game:GetService("Workspace").Game.WorkOut.Squats.Part.ClickDetector)
+                elseif _G.Stat == "Situp" then
+                    fireclickdetector(game:GetService("Workspace").Game.WorkOut.SitUp.Part.ClickDetector)
+                end;
+            end;
+        end;
+    end;
+end);
+
+page1:Drop("Stat",false,{"Treadmill","Push Up","Squat","Situp"},function(value)
+    _G.Stat = value
+end);
+
+local page2 = Tap1:newpage()
+
+page2:Label("Sir Cums Alot")
+
+page2:Button("Copy Discord", function()
+setclipboard("https://discord.gg/8XfrZSxyKk")
+create:Notifile("Thanks","Copied To Clipboard",3)
+end);
